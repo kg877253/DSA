4ac26ba3779d9a128d960a2f84232ffbfeda6c8d
Kartik Gupta <kg877253@gmail.com>
2026-07-22 22:35:11 +0530

    count char

--- Arrays and Strings/chararrays.cpp
+++ Arrays and Strings/chararrays.cpp
@@ -1,14 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int countchar(char ch[]){
+    int count=0;
+    
+    for(int i =0;i<ch.length();i++){
+
+    }
+
+}
 int main() {
 
     char ch[100];
-    
+    cout<<"Enter a string: ";
     cin>>ch;
-    
+    ch[2]='\0';
     
     cout<<ch[3]<<endl;
-    cout<<ch[5]<<endl;
+    cout<<ch;
     return 0;
 }
