5d640b75449ecf32d573d6c2a3f71735259d781a
Kartik Gupta <kg877253@gmail.com>
2026-07-28 21:31:39 +0530

    palindrome

---
+++ Arrays and Strings/validpalindrome.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char palindrome(string s ){
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return 'N';
+        }
+        left++;
+        right--;
+    }
+    return 'Y';
+}
+int main() {
+    string s = "racecar";
+    cout<<palindrome(s) << endl;
+    return 0;
+}
