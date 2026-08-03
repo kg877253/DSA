aebcbddf21764d25019ed953ef2b0053e177e5b6
Kartik Gupta <kg877253@gmail.com>
2026-08-03 20:52:16 +0530

    row wise manner

--- 2D Arrays/1st.cpp
+++ 2D Arrays/1st.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     //create a 2D array of size 3x4
+    // int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12}; It will take input in row wise manner
     int arr[3][4];
 
     //input values into the 2D array
