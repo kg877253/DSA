64ffa2d7276979a16df895d4f6295077d7eb3b2d
Kartik Gupta <kg877253@gmail.com>
2026-08-03 20:48:08 +0530

    2d Arrays input and output in C++.

--- 2D Arrays/1st.cpp
+++ 2D Arrays/1st.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
-    int arr[3][3]={{}};
+    //create a 2D array of size 3x4
+    int arr[3][4];
+
+    //input values into the 2D array
+    for(int i =0 ;i<3; i++){
+        for(int j = 0 ; j<4;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i =0 ;i<3; i++){
+        for(int j = 0 ; j<4;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
