b990e866f83fde70dbf19d1f44a3d18c33125b65
Kartik Gupta <kg877253@gmail.com>
2026-08-06 21:41:22 +0530

    to calculate the sum of each row in a 2D array. The program takes input for the number of rows and columns, then prompts the user to enter the elements of the array. After populating the array, it calculates and displays the sum of each row.

---
+++ 2D Arrays/rowsum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//to calc row wise sum of 2D array
+// void rowwisesum(int arr[][3],int row,int col){
+//     for(int row =0;row<3;row++){
+//         int sum=0;
+//         for(int col=0;col<3;col++){
+//             sum+=arr[row][col];
+//         }
+//         cout<<sum<<" ";
+//     }
+// }
+
+//to calc highest row sum of 2D array
+void rowwisehighsum(int arr[][4],int row,int col){
+    int maxi=-1;
+    int rowindex=-1;
+
+    for(int row=0;row<3;row++){
+        int sum=0;
+        for(int col=0;col<4;col++){
+            sum+=arr[row][col];
+        }
+
+        if(sum>maxi){
+            maxi=sum;
+            rowindex=row;
+        }
+    }
+    cout<<"highest row sum is "<<maxi<<" and its index is "<<rowindex<<endl;
+}
+
+
+int main() {
+    int arr[3][4];
+
+    cout<<"enter the elements of the array"<<endl;
+
+    for(int row =0;row<3;row++){
+        for(int col=0;col<4;col++){
+            cin>>arr[row][col];
+        }
+    }
+    // rowwisesum(arr, 3, 3);
+    rowwisehighsum(arr, 3, 4);
+    return 0;
+}
