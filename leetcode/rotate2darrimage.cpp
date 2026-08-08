c2d7e8849304e85569a155192c5e7095c243971e
Kartik Gupta <kg877253@gmail.com>
2026-08-08 22:02:42 +0530

    rotate2darrimage

--- leetcode/rotate2darrimage.cpp
+++ leetcode/rotate2darrimage.cpp
@@ -2,38 +2,48 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate(vector<vector<int>> &matrix)
+    {
         int n = matrix.size();
 
         // Step 1: Transpose (swap matrix[i][j] with matrix[j][i])
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
 
         // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     vector<vector<int>> matrix(n, vector<int>(n));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cin >> matrix[i][j];
         }
     }
     Solution sol;
     sol.rotate(matrix);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
