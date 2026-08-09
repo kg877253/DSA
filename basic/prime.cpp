beb1f58bda4705964b5a5bb609fd90dc0519072f
Kartik Gupta <kg877253@gmail.com>
2026-08-09 22:42:29 +0530

    prime

--- basic/prime.cpp
+++ basic/prime.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 
 bool isprime(int n ){
+
     if(n<=1){
         return false;
     }
     for(int i =2;i<n ;i++){
-
         if(n%i==0){
         return false;
-    }       
+     }       
 }
     return true;
 }
 int main() {
-
     int n ;
     cout<<"enter a number: ";
     cin>>n;
+
     if(isprime(n)){
         cout<<n<<" is prime number"<<endl;
-
     }
     else{
         cout<<n<<" is not prime number"<<endl;
     }
+    
     return 0;
 }
