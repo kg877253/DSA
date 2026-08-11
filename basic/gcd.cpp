7c0d1d5cb615378609a263526ac9755aa5af4195
Kartik Gupta <kg877253@gmail.com>
2026-08-11 22:48:03 +0530

    optimized

--- basic/gcd.cpp
+++ basic/gcd.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a,int b){
-    if(a==0) return b;
-    if(b==0) return a;
-    while(a!=b){
-        if(a>b){
-            a=a-b;
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
         }
-        else{
-            b=b-a;
+        else
+        {
+            b = b - a;
         }
     }
     return a;
 }
-int main() {
-    int a,b;
-    cout<<"enter two numbers: ";
-    cin>>a>>b;
+int main()
+{
+    int a, b;
+    cout << "enter two numbers: ";
+    cin >> a >> b;
 
-    int ans=gcd(a,b);
-    cout<<"gcd of "<<a<<" and "<<b<<" is: "<<ans<<endl;
+    int ans = gcd(a, b);
+    cout << "gcd of " << a << " and " << b << " is: " << ans << endl;
     return 0;
 }
