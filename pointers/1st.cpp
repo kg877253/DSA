b108778edddb5bf61826f95fb10499e0bf47cd06
Kartik Gupta <kg877253@gmail.com>
2026-08-13 22:09:15 +0530

    dereference operator is used to access the value at the address stored in a pointer. It is denoted by the asterisk (*) symbol. When you have a pointer variable, you can use the dereference operator to retrieve or modify the value it points to.

--- pointers/1st.cpp
+++ pointers/1st.cpp
@@ -4,7 +4,16 @@ using namespace std;
 int main() {
     int num=10;
     cout << "Value of num: " << num << endl;
-
     cout << "Address of num: " << &num << endl;
+
+    int *p=&num; // pointer variable p stores the address of num
+    cout << "Value of p (address of num): " << p << endl;
+    cout << "Value pointed by p: " << *p << endl;
+
+    double d=22.4;
+    double *p2=&d; // pointer variable p2 stores the address of d
+    cout << "Value of p2 (address of d): " << p2 << endl;
+    cout << "Value pointed by p2: " << *p2 << endl;
+
     return 0;
 }
