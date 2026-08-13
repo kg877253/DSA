4746dccfc88eea93b3b021524f964a6402e3e603
Kartik Gupta <kg877253@gmail.com>
2026-08-13 21:57:40 +0530

    startinng pointers

---
+++ pointers/1st.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num=10;
+    cout << "Value of num: " << num << endl;
+
+    cout << "Address of num: " << &num << endl;
+    return 0;
+}
